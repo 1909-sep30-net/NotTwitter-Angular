71add2ee6b14ef6f22b369deb29f84b14d15295c
PhatNguyen203 <Lee0ne203@gmail.com>
2019-11-07 13:20:41 -0600

    set up Auth0 --> can login and logout now

--- NotTwitterAngular/src/app/app.module.ts
+++ NotTwitterAngular/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
